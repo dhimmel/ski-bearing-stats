c5b52b32eafc1b395a738275db3a49af2b836b0f
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2024-03-07 22:16:54 -0500

    subplot_orientations

--- ski_bearings/utils.py
+++ ski_bearings/utils.py
@@ -1,10 +1,14 @@
 import json
 import logging
 import lzma
+import math
+import warnings
 from pathlib import Path
 from typing import Any, Literal
 
+import matplotlib.pyplot as plt
 import networkx as nx
+import osmnx
 import pandas as pd
 import requests
 from osmnx.bearing import add_edge_bearings
@@ -56,6 +60,7 @@ def load_ski_areas() -> pd.DataFrame:
 
 
 def get_ski_area_to_runs(runs: list[dict[str, Any]]) -> dict[str, Any]:
+    # using name is bad, duplicates like Black Mountain
     ski_area_to_runs: dict[str, Any] = {}
     for run in runs:
         if not (ski_areas := run["properties"]["skiAreas"]):
@@ -73,9 +78,10 @@ def create_networkx(runs: list[Any]) -> nx.MultiDiGraph:
     NOTE: Bearing distributions lack support for directed graphs
     https://github.com/gboeing/osmnx/issues/1137
     """
-    graph = nx.MultiDiGraph(crs="EPSG:4326")
+    graph = nx.MultiDiGraph(crs="EPSG:4326")  # https://epsg.io/4326
+    # filter out unsupported geometries like Polygons
+    runs = [run for run in runs if run["geometry"]["type"] == "LineString"]
     for run in runs:
-        assert run["geometry"]["type"] == "LineString"
         # NOTE: longitude comes before latitude in GeoJSON and osmnx, which is different than GPS coordinates
         for lon, lat, elevation in run["geometry"]["coordinates"]:
             graph.add_node((lon, lat), x=lon, y=lat, elevation=elevation)
@@ -95,3 +101,54 @@ def create_networkx(runs: list[Any]) -> nx.MultiDiGraph:
     graph = add_edge_bearings(graph)
     graph = add_edge_lengths(graph)
     return graph
+
+
+def subplot_orientations(groupings: dict[str, nx.MultiDiGraph]) -> plt.Figure:
+    """
+    Plot orientations from multiple graphs in a grid.
+    https://github.com/gboeing/osmnx-examples/blob/bb870c225906db5a7b02c4c87a28095cb9dceb30/notebooks/17-street-network-orientations.ipynb
+    """
+    # create figure and axes
+    n_groupings = len(groupings)
+    n_cols = math.ceil(n_groupings**0.5)
+    n_rows = math.ceil(n_groupings / n_cols)
+    figsize = (n_cols * 5, n_rows * 5)
+    fig, axes = plt.subplots(
+        n_rows, n_cols, figsize=figsize, subplot_kw={"projection": "polar"}
+    )
+
+    # plot each group's polar histogram
+    for ax, (name, graph) in zip(axes.flat, sorted(groupings.items()), strict=False):
+        with warnings.catch_warnings():
+            warnings.filterwarnings(
+                action="ignore",
+                message="edge bearings will be directional",
+                category=UserWarning,
+            )
+            fig, ax = osmnx.plot_orientation(
+                graph,
+                ax=ax,
+                title=name,
+                area=True,
+                weight="vertical",
+                color="#D4A0A7",
+            )
+            ax.title.set_size(18)
+            ax.yaxis.grid(False)
+    # hide axes for unused subplots
+    for ax in axes.flat[n_groupings:]:
+        ax.axis("off")
+    # add figure title and save image
+    # suptitle_font = {
+    #     "family": "DejaVu Sans",
+    #     "fontsize": 60,
+    #     "fontweight": "normal",
+    #     "y": 1,
+    # }
+    # fig.suptitle("City Street Network Orientation", **suptitle_font)
+    fig.tight_layout()
+    fig.subplots_adjust(hspace=0.35)
+    # fig.savefig("images/street-orientations.png", facecolor="w", dpi=100, bbox_inches="tight")
+    plt.close()
+    return fig
+    # plt.close()
